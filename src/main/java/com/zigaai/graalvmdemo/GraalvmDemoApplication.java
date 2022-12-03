a9a06d136041a6d35af69d97d90845591b234a01
zigaai <zigaai@qq.com>
2022-12-03 11:12:20 +0800

    2022-12-3

---
+++ src/main/java/com/zigaai/graalvmdemo/GraalvmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zigaai.graalvmdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraalvmDemoApplication.class, args);
+	}
+
+}
